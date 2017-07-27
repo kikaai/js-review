6378fbe1a8658d02d211e6f77ade381eb04cbc86
Karlyn Kaai <karlynkaai@Karlyns-MacBook-Pro.local>
2017-07-26 18:11:34 -1000

    completed exercise review string variables

--- exercises.js
+++ exercises.js
@@ -9,6 +9,15 @@ Now, unleash your coding beast!
 /*1.  Variables with a String as their value.
 Declare variables named yourName, neighborName, teacherName.  Console log each variable.*/
 
+var yourName = "Karlyn";
+var neighborName = "Eugene";
+var teacherName = "Vic";
+console.log(yourName);
+console.log(neighborName);
+console.log(teacherName);
+
+
+
 /*2. Variable with a Number as their value.
 Declare variables named numberOfFloorsBurjKhalifa, TokyoOlympicsYear. Please assign the actual data for each variable.
 Console log each variable.*/
