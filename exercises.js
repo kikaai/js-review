cf1a0eacdde5e6584bb8137926a2a6377284230c
Karlyn Kaai <karlynkaai@Karlyns-MacBook-Pro.local>
2017-07-26 18:15:31 -1000

    completed exercise boolean review

--- exercises.js
+++ exercises.js
@@ -28,6 +28,12 @@ console.log(TokyoOlympicsYear);
 
 /*3. Variables with Boolean values
 Declare variables named lovesPrepClass, eatsDonuts, drinksGin.  Console log each variable.*/
+var lovesPrepClass = true;
+var eatsDonuts = true;
+var drinksGin = true;
+console.log(lovesPrepClass);
+console.log(eatsDonuts);
+console.log(drinksGin);
 
 /*4. Variable with Null values
 Declare variables named completedCodingChallenge, traveledToSpace.  Console log each variable*/
