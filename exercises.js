a27004274a02d87a2ee67ac31a1ff502ef2f8eb4
Karlyn Kaai <karlynkaai@Karlyns-MacBook-Pro.local>
2017-07-26 18:21:35 -1000

    completed exercise review array's

--- exercises.js
+++ exercises.js
@@ -49,6 +49,11 @@ Console log the variable
 Console log the value at index 1.
 Console log the value at index 3.*/
 
+var japanPrefectures = ["ken", "fu", "hokkaido", "osaka", "tokyo"];
+console.log(japanPrefectures);
+console.log(japanPrefectures[1]);
+console.log(japanPrefectures[3]);
+
 /*6.  Variable with Array for its value.
 Declare a variable named planets and assign the value of each planet in our solar system, in the order of distance from the sun.  
 Console log the variable
